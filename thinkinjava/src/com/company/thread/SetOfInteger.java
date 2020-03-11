c4c2ce80d5a3928caff2a012bc65fa081796ed73
KollorSong <15632766111@163.com>
2020-03-12 00:11:11 +0800

    java之路

--- thinkinjava/src/com/company/thread/SetOfInteger.java
+++ thinkinjava/src/com/company/thread/SetOfInteger.java
@@ -1,18 +1,76 @@
 package com.company.thread;
 
-import java.util.HashSet;
-import java.util.Random;
+import java.util.*;
 
 public class SetOfInteger {
 
     public static void main(String[] args) {
-        Random random;
-        random = new Random();
-        HashSet<Integer> integers = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
-            integers.add(random.nextInt(30));
+//        Random random;
+//        random = new Random();
+//        HashSet<Integer> integers = new HashSet<>();
+//        for (int i = 0; i < 1000; i++) {
+//            integers.add(random.nextInt(30));
+//        }
+//        System.out.println(integers);
+
+
+        List list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
         }
-        System.out.println(integers);
+        for (Iterator iterator = list.iterator(); iterator.hasNext(); ) {
+            int i = (Integer) iterator.next();
+            System.out.println(i);
+        }
+
+
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
